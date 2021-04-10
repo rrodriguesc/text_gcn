d705c6961bdeef0ce54e2e0a7330cf6a8601647b
Rafael Carvalho <rrcarvalho@poli.ufrj.br>
2021-04-10 16:45:37 -0300

    Update text handler

--- twitter/data/create_corpus.py
+++ twitter/data/create_corpus.py
@@ -2,7 +2,8 @@ import re
 import pandas as pd
 
 pattern = re.compile(r'@[\w]+')
-pattern_link = re.compile(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)")
+pattern_link = re.compile(r"https?:\/\/(www\.)"
+                          r"?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)")
 
 
 def handle_data(row: pd.Series) -> str:
@@ -14,16 +15,18 @@ def handle_data(row: pd.Series) -> str:
     return text.strip()
 
 
-test_data = pd.read_csv('twitter/data/Test.csv', sep=';')
-train_data = pd.read_csv('twitter/data/Train100.csv', sep=';')
+if __name__ == "__main__":
 
-test_data['task'] = 'test'
-train_data['task'] = 'train'
+    test_data = pd.read_csv('twitter/data/Test.csv', sep=';')
+    train_data = pd.read_csv('twitter/data/Train100.csv', sep=';')
 
-print(f"test: {len(test_data)} train: {len(train_data)}")
-full_data = pd.concat([test_data, train_data], ignore_index=True)
-full_data['text'] = full_data.apply(handle_data, axis=1)
+    test_data['task'] = 'test'
+    train_data['task'] = 'train'
 
-full_data[['text']].to_csv('data/corpus/twt_pt.txt', sep='\t', header=False, index=False)
-full_data[['task', 'sentiment']].to_csv('data/twt_pt.txt', sep='\t', header=False)
-print('files saved')
+    print(f"test: {len(test_data)} train: {len(train_data)}")
+    full_data = pd.concat([test_data, train_data], ignore_index=True)
+    full_data['text'] = full_data.apply(handle_data, axis=1)
+
+    full_data[['text']].to_csv('data/corpus/twt_pt.txt', sep='\t', header=False, index=False)
+    full_data[['task', 'sentiment']].to_csv('data/twt_pt.txt', sep='\t', header=False)
+    print('files saved')
